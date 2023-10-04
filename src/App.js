3c827452b581257eec0bcf6df8ef905d9bc11e63
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2023-10-04 12:09:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Pick and Pack</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fulfillment - IGT code to cloud demo - token change
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
